3dc077daaf6956e48239bf8ae28dca466ac3c3d2
Julio Chazari <julio.chazari@gmail.com>
2020-05-19 07:08:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,11 +3,12 @@ def new_hash
 end
 
 def my_hash
-  {name: "Julio"}
+  {name: 'Julio'}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  {:name => 'Grace Hopper'
 end
 
 
